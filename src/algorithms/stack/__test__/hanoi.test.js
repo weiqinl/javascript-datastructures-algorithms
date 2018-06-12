8f96b2e97b28be868c08aeeea6f4e81a0ebd13c6
weiqinl <772863869@qq.com>
2018-06-12 16:04:42 +0800

    A: 增加  汉诺塔中圆盘移动的过程函数

--- src/algorithms/stack/__test__/hanoi.test.js
+++ src/algorithms/stack/__test__/hanoi.test.js
@@ -29,4 +29,31 @@ describe('hanoi', () => {
     expect(hanoi(5)).toEqual(31)
     expect(hanoi(15)).toEqual(32767)
   })
+
+  it('hanoiStackArray moves steps', () => {
+    let towers = ['A', 'B', 'C']
+
+    expect(hanoiStackArray(1, ...towers)).toEqual(
+      Array('第 1 步，将圆盘 1 从 A 移至 C; A: [],B: [],C: [1]')
+    )
+    expect(hanoiStackArray(2, ...towers)).toEqual(
+      Array(
+        '第 1 步，将圆盘 1 从 A 移至 B; A: [2],C: [],B: [1]',
+        '第 2 步，将圆盘 2 从 A 移至 C; A: [],B: [1],C: [2]',
+        '第 3 步，将圆盘 1 从 B 移至 C; B: [],A: [],C: [2,1]'
+      )
+    )
+
+    expect(hanoiStackArray(3, ...towers)).toEqual(
+      Array(
+        '第 1 步，将圆盘 1 从 A 移至 C; A: [3,2],B: [],C: [1]',
+        '第 2 步，将圆盘 2 从 A 移至 B; A: [3],C: [1],B: [2]',
+        '第 3 步，将圆盘 1 从 C 移至 B; C: [],A: [3],B: [2,1]',
+        '第 4 步，将圆盘 3 从 A 移至 C; A: [],B: [2,1],C: [3]',
+        '第 5 步，将圆盘 1 从 B 移至 A; B: [2],C: [3],A: [1]',
+        '第 6 步，将圆盘 2 从 B 移至 C; B: [],A: [1],C: [3,2]',
+        '第 7 步，将圆盘 1 从 A 移至 C; A: [],B: [],C: [3,2,1]'
+      )
+    )
+  })
 })
--- src/algorithms/stack/hanoi.js
+++ src/algorithms/stack/hanoi.js
@@ -11,6 +11,7 @@ export function hanoi(number) {
   // 4个      15次
   // 5个      31次
 
+  // f(n) = 2 * f(n-1) + 1
   let sum = 0 // 移动的步数0次
   if (number < 1) {
     return 0
@@ -22,7 +23,7 @@ export function hanoi(number) {
   }
 
   // 2的n次方减1
-  // sum = number === 1 ? 1 : 2 ** number - 1
+  // sum = 2 ** number - 1
 
   return sum
 }
@@ -62,14 +63,6 @@ function towerOfHanoi(
 ) {
   if (plates <= 0) {
     return moves
-  } else if (plates === 1) {
-    // 只有一个圆盘的情况
-    dest.push(source.pop())
-    const move = {}
-    move[sourceName] = source.toString()
-    move[helperName] = helper.toString()
-    move[destName] = dest.toString()
-    moves.push(move)
   } else {
     // 先将其他盘移动到辅助柱子上
     towerOfHanoi(
@@ -110,11 +103,11 @@ function towerOfHanoi(
  * 汉诺塔
  * 记录每一次圆盘移动之后，各个柱子上的圆盘排放顺序，完成汉诺塔。
  * @param {Int32Array} plates 圆盘的个数
- * @param {String} towerA 源柱子的名称
- * @param {String} towerB 辅助柱子的名称
- * @param {String} towerC 目标柱子的名称
+ * @param {String} sourceName 源柱子的名称
+ * @param {String} helperName 辅助柱子的名称
+ * @param {String} destName 目标柱子的名称
  */
-export function hanoiStack(plates, towerA, towerB, towerC) {
+export function hanoiStack(plates, sourceName, helperName, destName) {
   const source = new Stack() // 源
   const dest = new Stack() // 目的地
   const helper = new Stack() // 辅助
@@ -123,5 +116,110 @@ export function hanoiStack(plates, towerA, towerB, towerC) {
     source.push(i)
   }
 
-  return towerOfHanoi(plates, source, helper, dest, towerA, towerB, towerC)
+  return towerOfHanoi(
+    plates,
+    source,
+    helper,
+    dest,
+    sourceName,
+    helperName,
+    destName
+  )
+}
+
+/**
+ * 记录圆盘移动的过程
+ *
+ * @param {Int32Array} plates 圆盘个数
+ * @param {Array} source 源柱子
+ * @param {Array} helper 辅助柱子
+ * @param {Array} dest 目的地柱子
+ * @param {String} sourceName 源柱子的名字
+ * @param {String} helperName 辅助柱子的名字
+ * @param {String} destName 目的地柱子的名字
+ * @param {Array} moves 步骤存储器，存储每一步的流程
+ */
+function moveOfHanoi(
+  plates,
+  source,
+  helper,
+  dest,
+  sourceName,
+  helperName,
+  destName,
+  moves = []
+) {
+  if (plates <= 0) {
+    return moves
+  } else if (plates === 1) {
+    // 弹出源柱子上剩下的最大圆盘，并将其压入目标柱子
+    dest.push(source.pop())
+    num++
+    let sourceArr = source.toString()
+    let helperArr = helper.toString()
+    let destArr = dest.toString()
+    let movestr = `第 ${num} 步，将圆盘 ${plates} 从 ${sourceName} 移至 ${destName}; ${sourceName}: [${sourceArr}],${helperName}: [${helperArr}],${destName}: [${destArr}]`
+    moves.push(movestr)
+  } else {
+    moveOfHanoi(
+      plates - 1,
+      source,
+      dest,
+      helper,
+      sourceName,
+      destName,
+      helperName,
+      moves
+    )
+
+    // 弹出源柱子上剩下的最大圆盘，并将其压入目标柱子
+    dest.push(source.pop())
+    num++
+    let sourceArr = source.toString()
+    let helperArr = helper.toString()
+    let destArr = dest.toString()
+    let movestr = `第 ${num} 步，将圆盘 ${plates} 从 ${sourceName} 移至 ${destName}; ${sourceName}: [${sourceArr}],${helperName}: [${helperArr}],${destName}: [${destArr}]`
+    moves.push(movestr)
+
+    moveOfHanoi(
+      plates - 1,
+      helper,
+      source,
+      dest,
+      helperName,
+      sourceName,
+      destName,
+      moves
+    )
+  }
+  return moves
+}
+
+let num = 0 // 记录移动的次数
+/**
+ * 汉诺塔
+ * 记录每一次圆盘移动的动作。从${源柱子}到${目标柱子}
+ * @param {Int32Array} plates 圆盘的个数
+ * @param {String} sourceName 源柱子的名称
+ * @param {String} helperName 辅助柱子的名称
+ * @param {String} destName 目标柱子的名称
+ */
+export function hanoiStackArray(plates, sourceName, helperName, destName) {
+  let source = new Stack()
+  let helper = new Stack()
+  let dest = new Stack()
+
+  for (let i = plates; i > 0; i--) {
+    source.push(i)
+  }
+  num = 0
+  return moveOfHanoi(
+    plates,
+    source,
+    helper,
+    dest,
+    sourceName,
+    helperName,
+    destName
+  )
 }
